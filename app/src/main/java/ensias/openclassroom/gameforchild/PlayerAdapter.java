e30d06e68a84e97dd74d1918eccfe0ba296d20c8
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-03-27 22:33:49 +0100

    create new sessionfor user

--- app/src/main/java/ensias/openclassroom/gameforchild/PlayerAdapter.java
+++ app/src/main/java/ensias/openclassroom/gameforchild/PlayerAdapter.java
@@ -3,6 +3,7 @@ package ensias.openclassroom.gameforchild;
 
 import android.accounts.Account;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
@@ -15,9 +16,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 import ensias.openclassroom.gameforchild.beans.Player;
+import ensias.openclassroom.gameforchild.beans.Session;
 
 public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.MyViewHolder> {
     public static ArrayList<Player> listPlayer = new ArrayList();
+    public static Context parent2 ;
    // public static Context parent2 ;
 
     @Override
@@ -30,7 +33,7 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.MyViewHold
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //pour créer un laouyt depuis un XML
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-       // parent2 = parent.getContext();
+        parent2 = parent.getContext();
         View view = inflater.inflate(R.layout.item, parent, false);
         return new MyViewHolder(view);
     }
@@ -51,16 +54,13 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.MyViewHold
             super(itemView);
             name = itemView.findViewById(R.id.name);
             profil = itemView.findViewById(R.id.profil);
-
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                   /* Intent intent = new Intent(parent2, DetailsEtablissement.class);
-                    intent.putExtra("name",currentEtab.getName());
-                    intent.putExtra("label",currentEtab.getLabel());
-                    intent.putExtra("latitude",(Double)currentEtab.getLatitude());
-                    intent.putExtra("longitude",(Double)currentEtab.getLongitude());
-                    parent2.startActivity(intent);*/
+                   //open session
+                    Session.setPlayer(currentPlayer);
+                    Intent intent = new Intent(parent2, HomeSession.class);
+                    parent2.startActivity(intent);
                 }
             });
 
@@ -69,32 +69,10 @@ public class PlayerAdapter extends RecyclerView.Adapter<PlayerAdapter.MyViewHold
         public void display(Player player) {
             currentPlayer = player;
             name.setText(player.getNom());
-
-
-            //traitement image
-           // Bitmap bmp = BitmapFactory.decodeByteArray(player.getImage_profil(), 0, player.getImage_profil().length);
-           // profil.setImageBitmap(Bitmap.createScaledBitmap(bmp, profil.getWidth(), profil.getHeight(), false));
-           // profil.setImageResource(android.R.color.transparent);
-            //fin traitement image
-
             Bitmap bitmap = BitmapFactory.decodeByteArray(player.getImage_profil(), 0, player.getImage_profil().length);
-
-
             profil.setImageBitmap(bitmap.createScaledBitmap(bitmap, 20, 20, true));
          //   bitmap.createScaledBitmap(bitmap,(int)(bitmap.getWidth()*0.5), (int)(bitmap.getHeight()*0.5), true);
          //   profil.setImageBitmap(bitmap);
-
-            /*
-            *    BitmapDrawable drawable = (BitmapDrawable) imageUpload.getDrawable();
-                    Bitmap bmap = drawable.getBitmap();
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    bmap.compress(Bitmap.CompressFormat.PNG,100,bos);
-                    byte[] imageOnByte = bos.toByteArray();
-                    player.setImage_profil(imageOnByte);
-                    *
-            * */
-
-
         }
     }
 
---
+++ app/src/main/java/ensias/openclassroom/gameforchild/beans/Session.java
@@ -0,0 +1,15 @@
+package ensias.openclassroom.gameforchild.beans;
+
+public class Session {
+    public static Player player;
+
+
+
+    public static Player getPlayer() {
+        return player;
+    }
+
+    public static void setPlayer(Player player) {
+        Session.player = player;
+    }
+}
