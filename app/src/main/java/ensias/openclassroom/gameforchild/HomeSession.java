f5b4bf22422f5c70fa7269235674eafc20df80a7
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-03-27 23:07:54 +0100

    premiere partie home session

--- app/src/main/java/ensias/openclassroom/gameforchild/HomeSession.java
+++ app/src/main/java/ensias/openclassroom/gameforchild/HomeSession.java
@@ -3,11 +3,16 @@ package ensias.openclassroom.gameforchild;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import ensias.openclassroom.gameforchild.beans.Session;
 
 public class HomeSession extends AppCompatActivity {
-
+    private TextView messageWelcome ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        messageWelcome = findViewById(R.id.messageWelcome);
+        messageWelcome.setText("Welcome "+ Session.getPlayer().getNom());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_session);
     }
