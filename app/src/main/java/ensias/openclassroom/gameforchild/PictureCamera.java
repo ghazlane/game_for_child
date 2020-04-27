9d659f10a8dc8605468e93b2fe858719280a7338
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-04-27 23:16:44 +0000

    Connexion with server

--- app/src/main/java/ensias/openclassroom/gameforchild/PictureCamera.java
+++ app/src/main/java/ensias/openclassroom/gameforchild/PictureCamera.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class PictureCamera extends AppCompatActivity {
 
     private ImageView photoCamera;
     public static Bitmap PICTURE_BITMAP ;
-
-
+    public TextView answer3 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,60 @@ public class PictureCamera extends AppCompatActivity {
         setContentView(R.layout.activity_picture_camera);
         photoCamera = findViewById(R.id.imagePicture);
         photoCamera.setImageResource(R.drawable.level1_ballon);
-
+        answer3 = findViewById(R.id.reponsechoix3_picture);
+        //Connexion with server
         photoCamera.setImageBitmap(PICTURE_BITMAP);
+        answer3.setText("test");
+        connectServer();
     }
+
+    void connectServer(){
+        System.out.println("********************* test connect server");
+        String postUrl= "http://192.168.1.3:5000/";
+        String postBodyText="Hello";
+        MediaType mediaType = MediaType.parse("text/plain; charset=utf-8");
+        RequestBody postBody = RequestBody.create(mediaType, postBodyText);
+        postRequest(postUrl, postBody);
+    }
+
+    void postRequest(String postUrl, RequestBody postBody) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Cancel the post on failure.
+                call.cancel();
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        System.out.println("Failed to Connect to Server");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            answer3.setText(response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+
 }
