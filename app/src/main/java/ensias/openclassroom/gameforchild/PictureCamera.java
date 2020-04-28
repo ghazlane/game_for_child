b909f72b1c371114be9a21aa160777cd112407be
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-04-28 00:15:54 +0000

    Server configuration and copy image

--- app/src/main/java/ensias/openclassroom/gameforchild/PictureCamera.java
+++ app/src/main/java/ensias/openclassroom/gameforchild/PictureCamera.java
@@ -2,17 +2,22 @@ package ensias.openclassroom.gameforchild;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -38,31 +43,45 @@ public class PictureCamera extends AppCompatActivity {
     }
 
     void connectServer(){
-        System.out.println("********************* test connect server");
+
         String postUrl= "http://192.168.1.3:5000/";
-        String postBodyText="Hello";
-        MediaType mediaType = MediaType.parse("text/plain; charset=utf-8");
-        RequestBody postBody = RequestBody.create(mediaType, postBodyText);
-        postRequest(postUrl, postBody);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        // Read BitMap by file path
+        PICTURE_BITMAP.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+        RequestBody postBodyImage = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray))
+                .build();
+
+        answer3.setText("Please wait ...");
+        postRequest(postUrl, postBodyImage);
     }
 
     void postRequest(String postUrl, RequestBody postBody) {
+
         OkHttpClient client = new OkHttpClient();
+
         Request request = new Request.Builder()
                 .url(postUrl)
                 .post(postBody)
                 .build();
+
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 // Cancel the post on failure.
                 call.cancel();
+
                 // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-
-                        System.out.println("Failed to Connect to Server");
+                        answer3.setText("Failed to Connect to Server");
                     }
                 });
             }
@@ -83,7 +102,4 @@ public class PictureCamera extends AppCompatActivity {
             }
         });
     }
-
-
-
 }
